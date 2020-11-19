0c9dc14b1a8ed5882c556d514370eb2a656f1c98
László Trautsch <trautschlaci@gmail.com>
2020-11-19 23:55:06 +0100

    bugfixes

--- Assets/Io_Framework/Scripts/PlayerNamePresenter.cs
+++ Assets/Io_Framework/Scripts/PlayerNamePresenter.cs
@@ -8,12 +8,18 @@ public class PlayerNamePresenter : MonoBehaviour
     public Player player;
     public TextMeshPro nameText;
 
+    private int direction = 1;
+
     public virtual void Update()
     {
         nameText.text = player.PlayerName;
         nameText.transform.rotation = Camera.main.transform.rotation;
         var scale = nameText.transform.localScale;
-        scale.x = player.transform.localScale.x;
+        if (player.transform.localScale.x * direction < 0)
+        {
+            direction *= -1;
+            scale.x *= -1;
+        }
         nameText.transform.localScale = scale;
     }
 }
