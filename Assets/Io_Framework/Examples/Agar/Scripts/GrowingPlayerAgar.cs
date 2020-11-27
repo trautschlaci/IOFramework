c19560fcc7150ef2ffadecb99b0d53156956b29c
László Trautsch <trautschlaci@gmail.com>
2020-11-27 21:07:53 +0100

    Growing player update

---
+++ Assets/Io_Framework/Examples/Agar/Scripts/GrowingPlayerAgar.cs
@@ -0,0 +1,60 @@
+﻿using Mirror;
+using TMPro;
+using UnityEngine;
+
+namespace Io_Framework.Examples.Agar
+{
+    public class GrowingPlayerAgar : GrowingPlayer
+    {
+        public TextMeshPro NameText;
+
+
+        private SpriteRenderer _sprite;
+
+
+        [Server]
+        protected override float CalculateSizeFromScore(int score)
+        {
+            return Mathf.Sqrt(1.0f + score / (2.0f * Mathf.PI));
+        }
+
+
+
+        public float GetCameraScale()
+        {
+            return CalculateCameraScale(transform.localScale.x);
+        }
+
+        protected override float CalculateCameraScale(float size)
+        {
+            return 3 + size;
+        }
+
+
+
+
+        protected override void Start()
+        {
+            base.Start();
+
+
+            if (!isClient)
+                return;
+
+            _sprite = GetComponent<SpriteRenderer>();
+        }
+
+        [ClientCallback]
+        private void Update()
+        {
+            _sprite.sortingOrder = Score.Score;
+            NameText.sortingOrder = Score.Score;
+
+
+            if (Camera.main == null || Camera.main.transform.parent != transform)
+                return;
+
+            Camera.main.orthographicSize = CalculateCameraScale(transform.localScale.x);
+        }
+    }
+}
---
+++ Assets/Io_Framework/Scripts/GrowingPlayer.cs
@@ -0,0 +1,58 @@
+﻿using Mirror;
+using UnityEngine;
+
+namespace Io_Framework
+{
+    public abstract class GrowingPlayer : NetworkBehaviour
+    {
+        public float ViewRangeScaler = 3.0f;
+        public bool Is2D = true;
+        
+
+        protected PlayerScore Score;
+        protected Player OwnPlayer;
+
+
+        private ReverseProximityChecker _reverseProximityChecker;
+
+
+        [Server]
+        protected virtual void ChangeSizeServer(int oldScore, int newScore)
+        {
+            var lastSize = transform.localScale.x;
+            var newSize = CalculateSizeFromScore(newScore);
+
+            if (Is2D)
+                transform.localScale = new Vector3(newSize, newSize, 1.0f);
+            else
+                transform.localScale = new Vector3(newSize, newSize, newSize);
+            
+
+            OwnPlayer.ViewRange = ViewRangeScaler * CalculateCameraScale(newSize);
+            if (_reverseProximityChecker != null)
+            {
+                _reverseProximityChecker.OwnExtent *= (newSize/lastSize);
+            }
+        }
+
+        protected abstract float CalculateSizeFromScore(int score);
+
+
+
+        protected abstract float CalculateCameraScale(float size);
+
+        protected virtual void Start()
+        {
+            Score = GetComponent<PlayerScore>();
+            OwnPlayer = GetComponent<Player>();
+
+
+            if (!isServer)
+                return;
+
+            _reverseProximityChecker = GetComponent<ReverseProximityChecker>();
+            Score.OnScoreChangedServer += ChangeSizeServer;
+            ChangeSizeServer(0, Score.Score);
+        }
+    }
+}
